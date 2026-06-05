5738a6a2c2a4d368a2a18e6c2d9bcc5e8fd14592
Abhishek Gupta <103306891+abhishekgupta1907@users.noreply.github.com>
2026-06-05 16:24:27 +0530

    Add components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import About from "../components/About";
-import Skills from "../components/Skills";
-import Experience from "../components/Experience";
-import Projects from "../components/Projects";
-import Contact from "../components/Contact";
-import Footer from "../components/Footer";
-import ScrollTop from "../components/ScrollTop";
+import Navbar from "../Components/Navbar";
+import Hero from "../Components/Hero";
+import About from "../Components/About";
+import Skills from "../Components/Skills";
+import Experience from "../Components/Experience";
+import Projects from "../Components/Projects";
+import Contact from "../Components/Contact";
+import Footer from "../Components/Footer";
+import ScrollTop from "../Components/ScrollTop";
 
 const Home = () => {
     return (
